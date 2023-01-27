74c8d410bb5bbe241795d5885856e8a23b12ca16
zhanxin <luozhanxin27@gmail.com>
2023-01-27 15:42:45 +0800

    feat: add oss config

--- src/config/envs/dev.js
+++ src/config/envs/dev.js
@@ -23,4 +23,10 @@ module.exports = {
   corsOrigin: "*",
   // 短信发送过期时间
   msgVeriCodeTimeout: "60",
+
+  // oss config
+  tencentOssSecretId: "AKIDU0X01XRnRZQlrDQmeSSy0jseJbQ02JRw",
+  tencentOssSecretKey: "E14hmNUKdZI2LCYSqmA9ffMQz1chZiJt",
+  ossRegion: "ap-nanjing",
+  ossBucket: "monk-1251844408",
 };
